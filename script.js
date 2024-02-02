a563454a4aae59483f4e11c24fae0b68bf7b7691
Vivek Rai <108179025+vivekrai7272@users.noreply.github.com>
2024-02-03 01:08:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "dfbe78a6da08468fbc3243d19b029a90";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
